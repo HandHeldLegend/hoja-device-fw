5b5d95554576401fbcadd4065ae251c962bf7df4
Mitch Cairns <mitchellcairns@gmail.com>
2025-08-04 22:55:42 -0700

    Undo accidental FW changes

--- boards/progcc_3.2/timestamp.h
+++ boards/progcc_3.2/timestamp.h
@@ -2,6 +2,6 @@
 #define BUILD_TIMESTAMP_H
 
 #include <stdint.h>
-#define BUILD_TIMESTAMP ((uint32_t)1754367627)
+#define BUILD_TIMESTAMP ((uint32_t)1754373057)
 
 #endif /* BUILD_TIMESTAMP_H */
--- boards/super_gamepad/timestamp.h
+++ boards/super_gamepad/timestamp.h
@@ -2,6 +2,6 @@
 #define BUILD_TIMESTAMP_H
 
 #include <stdint.h>
-#define BUILD_TIMESTAMP ((uint32_t)1754367364)
+#define BUILD_TIMESTAMP ((uint32_t)1754373074)
 
 #endif /* BUILD_TIMESTAMP_H */
