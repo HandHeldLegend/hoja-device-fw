a0151a16f4e043e8814d1fb618dd852d3f54dfb3
Mitch Cairns <mitchellcairns@gmail.com>
2025-12-22 22:21:47 -0800

    board config update

--- boards/gcu_r4k/board_config.h
+++ boards/gcu_r4k/board_config.h
@@ -95,10 +95,10 @@ extern adc_driver_cfg_s user_adc_hal;
 #define HOJA_HAPTICS_CHAN_A_PIN     8
 #define HOJA_HAPTICS_CHAN_B_PIN     3
 
-#define HOJA_HAPTICS_MAX        0.5f
+#define HOJA_HAPTICS_MAX        0.625f
 
-#define HOJA_HAPTICS_MIN_LO     0.085f
-#define HOJA_HAPTICS_MIN_HI     0.085f
+#define HOJA_HAPTICS_MIN_LO     0.135f
+#define HOJA_HAPTICS_MIN_HI     0.10f
 
 #define HOJA_HAPTICS_DEBUG 0
 // ---------------------------------
