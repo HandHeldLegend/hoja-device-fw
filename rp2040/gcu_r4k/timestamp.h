e74c67fed79504b7a0d31aec9bfa30e1d5f50f9c
Mitch Cairns <mitchellcairns@gmail.com>
2026-07-15 11:50:09 -0700

    Scaler Defaults
    Fix scaler to default when analog calibration is run

--- rp2040/gcu_r4k/timestamp.h
+++ rp2040/gcu_r4k/timestamp.h
@@ -2,6 +2,6 @@
 #define BUILD_TIMESTAMP_H
 
 #include <stdint.h>
-#define BUILD_TIMESTAMP ((uint32_t)1784140759)
+#define BUILD_TIMESTAMP ((uint32_t)1784141344)
 
 #endif /* BUILD_TIMESTAMP_H */
--- rp2040_w/pico_w/timestamp.h
+++ rp2040_w/pico_w/timestamp.h
@@ -2,6 +2,6 @@
 #define BUILD_TIMESTAMP_H
 
 #include <stdint.h>
-#define BUILD_TIMESTAMP ((uint32_t)1784140817)
+#define BUILD_TIMESTAMP ((uint32_t)1784141337)
 
 #endif /* BUILD_TIMESTAMP_H */
